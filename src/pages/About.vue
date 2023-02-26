f645252132612d9edb3b4469527da00c71971b9a
ayubkhan-x <khakimovayubkhan@gmail.com>
2023-02-26 18:09:23 +0500

    init

---
+++ src/pages/About.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="page-wrapper">
+    <div class="container">I am about page!</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "v-About",
+};
+</script>
---
+++ src/pages/Faq.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="page-wrapper">
+    <div class="container">I am about FAQ!</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "v-Faq",
+};
+</script>
